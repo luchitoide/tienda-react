c86d3bd466390ed74c669bb3721d5825d03b8023
luchitoide <luchitoiderp@gmail.com>
2023-08-26 12:37:27 -0500

    api updated

--- src/components/OrderItem.jsx
+++ src/components/OrderItem.jsx
@@ -12,7 +12,7 @@ const OrderItem = ({ product }) => {
 	return (
 		<div className="OrderItem">
 			<figure>
-				<img src={product.images[0]} alt={product.title} />
+				<img src={product.image} alt={product.title} />
 			</figure>
 			<p>{product.title}</p>
 			<p>${product.price}</p>
--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -12,7 +12,7 @@ const ProductItem = ({ product }) => {
 
 	return (
 		<div className="ProductItem">
-			<img src={product.images[0]} alt={product.title} />
+			<img src={product.image} alt={product.title} />
 			<div className="product-info">
 				<div>
 					<p>${product.price}</p>
